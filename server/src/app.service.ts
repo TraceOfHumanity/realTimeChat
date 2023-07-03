2236cdc6976b88933ac107a43fe84402c35fe257
Alexander_Ovchar <azalexxx02@gmail.com>
2023-07-03 16:56:59 +0300

    working

---
+++ server/src/app.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { Prisma, Chat } from '@prisma/client';
+
+@Injectable()
+export class AppService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createMessage(data: Prisma.ChatCreateInput): Promise<Chat> {
+    return await this.prisma.chat.create({ data });
+  }
+
+  async getMessages(): Promise<Chat[]> {
+    return await this.prisma.chat.findMany();
+  }
+  getHello(): string {
+    return 'Hello World!';
+  }
+}
